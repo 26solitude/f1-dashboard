56abf887f73daa42da3afd6c1a168917b9c30040
26solitude <26dev@naver.com>
2025-12-06 22:55:06 +0900

    Initial commit

---
+++ src/test/java/com/hojang/f1dashboard/F1DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hojang.f1dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1DashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
